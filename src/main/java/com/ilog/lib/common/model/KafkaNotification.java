56b451c06e2eaa7816b2bfa4425caea3fd45b2ad
aboshady <ahmed.aboshady404@gmail.com>
2024-10-28 12:44:30 +0200

    enhance KafkaNotification class

--- src/main/java/com/ilog/lib/common/model/KafkaNotification.java
+++ src/main/java/com/ilog/lib/common/model/KafkaNotification.java
@@ -2,6 +2,8 @@ package com.ilog.lib.common.model;
 
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,14 +12,16 @@ import java.util.List;
 
 @NoArgsConstructor
 @Data
+@Builder
+@AllArgsConstructor
 public class KafkaNotification {
 
     private List<String> tokens;
     private boolean admin;
     private List<Long> userIds;
-    String title;
-    String body;
-    Object payload;
+    private String title;
+    private String body;
+    private Object payload;
 
 
 }
