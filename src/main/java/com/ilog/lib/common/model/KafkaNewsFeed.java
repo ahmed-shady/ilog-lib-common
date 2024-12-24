9317b57dc7325d99efbeaba6c94427d77be32f01
aboshady <ahmed.aboshady404@gmail.com>
2024-12-24 23:15:49 +0200

    version 0.0.12: add auditing to KafkaNewsFeed

--- src/main/java/com/ilog/lib/common/model/KafkaNewsFeed.java
+++ src/main/java/com/ilog/lib/common/model/KafkaNewsFeed.java
@@ -7,6 +7,8 @@ import com.ilog.lib.common.constant.ShareSetting;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.time.Instant;
+
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -26,5 +28,18 @@ public class KafkaNewsFeed extends KafkaBaseMessage {
     private Long doctorId;
 
     private Long consultantId;
+
+
+    private Instant createdAt;
+
+
+    private Instant updatedAt;
+
+
+
+    private Long createdById;
+
+
+    private Long updatedById;
 
 }
